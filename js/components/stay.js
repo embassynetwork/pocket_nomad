2c908ee04cc00a1e152fa8128465c75d683ba569
craigambrose <craig@craigambrose.com>
2016-08-31 10:56:12 +0200

    added stay component

---
+++ js/components/stay.js
@@ -0,0 +1,22 @@
+// @flow
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Stay extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello world</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
