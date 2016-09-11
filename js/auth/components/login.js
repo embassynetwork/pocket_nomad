8133ce689087347a8066a84bc91a09396db1b142
craigambrose <craig@craigambrose.com>
2016-09-11 11:05:15 -0700

    working on login screen

---
+++ js/auth/components/login.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View>
+        <Text>this is the login screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+});
--- js/components/main.js
+++ js/components/main.js
@@ -1,11 +1,12 @@
 // @flow
 import React, { Component } from 'react';
 import Stays from './stays/stays'
+import Login from '../auth/components/login'
 
 export default class Main extends Component {
   render() {
     return (
-      <Stays />
+      <Login />
     )
   }
 }
