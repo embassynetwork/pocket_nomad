66bf2ea475d138eb697cda80f29140e237b4046b
craigambrose <craig@craigambrose.com>
2016-09-11 10:50:48 -0700

    using redux devtools

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 // @flow
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import Main from './components/main'
 import * as reducers from './reducers';
+import devTools from 'remote-redux-devtools';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
+
+const initialState = {default: {auth: {foo: 'bar'}}}
+
+function configureStore(initialState) {
+  const enhancer = compose(
+    applyMiddleware(thunk),
+    devTools()
+  );
+  // Note: passing enhancer as last argument requires redux@>=3.1.0
+  const store = createStore(reducer, initialState, enhancer);
+  // If you have other enhancers & middlewares
+  // update the store after creating / changing to allow devTools to use them
+  devTools.updateStore(store);
+  return store;
+}
+
+const store = configureStore(initialState)
 
 export default class App extends Component {
   render() {
