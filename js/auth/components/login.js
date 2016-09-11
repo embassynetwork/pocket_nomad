780ae935b3daaa5366b6be00239c5e90f70a09fa
craigambrose <craig@craigambrose.com>
2016-09-11 12:36:02 -0700

    working on login page

--- js/auth/components/login.js
+++ js/auth/components/login.js
@@ -2,15 +2,42 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
+import TextField from 'react-native-md-textinput';
+
 export default class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>this is the login screen</Text>
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>a global housing layer for the new world</Text>
+          </View>
+          <TextField label={'Name'} highlightColor={'#ecf0f1'} />
+          <TextField label={'Password'} highlightColor={'#ecf0f1'} />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#c0392b",
+  },
+  body: {
+    flex: 1,
+    margin: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  header: {
+    height: 200,
+    // borderWidth: 1,
+  },
+  headerText: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    textAlign: 'center',
+  }
 });
